45bd1cf0a6e973bb87c1a8a430a7609dd3edbeda
gsbakshi <greaser.bakshi@gmail.com>
2021-12-02 09:17:28 +0530

    Error Handling for prompt values

--- 03-monster-killer/app.js
+++ 03-monster-killer/app.js
@@ -22,18 +22,28 @@ const LOG_EVENTS = {
     GAME_OVER: 'GAME_OVER',
 };
 
-const enteredValue = prompt('Maximum life for you and the monster', '100');
+const getMaxLifeValue = () => {
+    const enteredValue = prompt('Maximum life for you and the monster', '100');
+    const parsedValue = parseInt(enteredValue);
+    if (isNaN(parsedValue) || parsedValue <= 0) {
+        throw { message: 'Invalid input, not a valid number' };
+    }
+    return parsedValue;
+};
 
-let chosenMaxLife = parseInt(enteredValue);
+let chosenMaxLife;
+try {
+    chosenMaxLife = getMaxLifeValue();
+} catch (error) {
+    chosenMaxLife = 100;
+    console.log(error);
+}
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
 let battleLog = [];
 let lastLoggedEntry;
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
-    chosenMaxLife = 100;
-}
-
-let currentMonsterHealth = chosenMaxLife;
-let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
